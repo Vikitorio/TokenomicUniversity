6883175fdfc8cd02b08c627a7b35c70bd63c4616
Viktor M <mikhalcheskiu@gmail.com>
2022-12-14 12:10:12 +0300

    add:update userTemplate and TokenExchange

--- src/main/scala/Classes/TokenExchange.scala
+++ src/main/scala/Classes/TokenExchange.scala
@@ -1,11 +1,52 @@
 package Classes
 
-object TokenExchange{
-  private var _tokentBank = 1000000.0d
+object TokenExchange {
+  private var _tokenBank = 1000000.0d
   private var _fiatBank = 1000000.0d
-  private var _currentPrice :Double = 0.1d
+  private var _currentPrice: Double = 0.1d
+  private var _minPrice: Double = 0.1d
+  private var _maxPrice: Double = 5d
+  private var _oneTokenChange = 0.01d
+  private var _sellStack:Double = 0d
+  private var _buyStack:Double = 0d
 
-  def TokenBank: Double = _tokentBank
-  def FiatBank:Double= _fiatBank
-  def sellToken(User:UserTemplate): Boolean = {return false}
+
+  def sellTokens(user: UserTemplate, amount: Double): Boolean ={
+    user.TokenBalance -= amount
+    user.FiatBalance += amount * _currentPrice
+    _currentPrice -= amount * _oneTokenChange
+    _sellStack += amount
+    priceSupport()
+    return true
+  }
+  def buyTokens(user: UserTemplate, amount: Double): Boolean = {
+    if (user.FiatBalance / _currentPrice < amount)  false
+    user.FiatBalance - amount * _currentPrice
+    user.TokenBalance += amount
+    _currentPrice += amount * _oneTokenChange
+    _buyStack += amount
+    priceSupport()
+    return true
+  }
+  def updatePrice(): Unit ={
+    if(_sellStack !=0 && _buyStack != 0) {
+      _currentPrice *= _buyStack / _sellStack
+      priceSupport()
+    }
+  }
+  def priceSupport(): Unit ={
+    if(_currentPrice<_minPrice) {
+      var priceDifference = _minPrice - _currentPrice
+      _tokenBank += priceDifference / _oneTokenChange
+      _fiatBank -= (priceDifference / _oneTokenChange)* ((_currentPrice + _minPrice)/2)
+    }
+    else if (_currentPrice > _maxPrice) {
+      var priceDifference =  _currentPrice - _maxPrice
+      _tokenBank -= priceDifference / _oneTokenChange
+      _fiatBank += (priceDifference / _oneTokenChange) * ((_currentPrice + _minPrice) / 2)
+    }
+  }
+  def TokenBank: Double = _tokenBank
+  def FiatBank: Double = _fiatBank
+  def CurrentPrice:Double = _currentPrice
 }
--- src/main/scala/Classes/UserTemplate.scala
+++ src/main/scala/Classes/UserTemplate.scala
@@ -2,22 +2,30 @@ package Classes
 import scala.util.Random
 abstract class UserTemplate (name:String = "-",surname:String = "-",age:String = "-",username:String = "-",email:String = "-",password:String = "-"){
   protected var random = new Random()
-  protected var _fiatWalet = 300d;
-  protected var _tokenWalet = 125.0d;
+  protected var _fiatBalance = 300d;
+  protected var _tokenBalance = 125.0d;
   protected var _name: String = name;
   protected var _surname: String = surname;
   protected var _age: String = age;
   protected var _email: String = email;
-  protected var _greedy = random.between(1,5)
+  protected var _greed: Int = random.between(1, 5)
 
-  override def toString : String = s"${name} ${surname} | Fiat: ${_fiatWalet} | Tokens: ${_tokenWalet}"
-  def buyTokens(payment:Double): Boolean ={TokenExchange.sellToken(this)}
-  def sellToken(): Unit ={
+  override def toString : String = s"${name} ${surname} | Fiat: ${_fiatBalance} | Tokens: ${_tokenBalance}"
+  def buyTokens(amount:Double): Boolean ={TokenExchange.buyTokens(this, amount)}
 
+  def sellToken(amount: Double): Boolean = {
+    if (TokenExchange.sellTokens(this, amount)) false
+    true
   }
   def getTokens(tokens:Double): Unit ={
-    _tokenWalet += tokens
+    _tokenBalance += tokens
   }
 
+  def FiatBalance: Double = _fiatBalance
+  def TokenBalance:Double = _tokenBalance
+  def Name: String = _name
+  def Surname: String = _surname
   def Age: String = _age
+  def Email:String = _email
+  def Greed:Int = _greed
 }
